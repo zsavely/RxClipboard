2ca8bb69ee7b5f6b174eebf5b4391ee306583186
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-08-09 15:33:03 +0300

    Added tests to check if the constructor is private.

--- rxclipboard/src/test/java/com/szagurskii/rxclipboard/PreconditionsTests.java
+++ rxclipboard/src/test/java/com/szagurskii/rxclipboard/PreconditionsTests.java
@@ -5,14 +5,18 @@ import com.szagurskii.rxclipboard.internal.Preconditions;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Savelii Zagurskii
  */
 public class PreconditionsTests {
-  @Before
-  public void setup() {
+  @Before public void setup() {
   }
 
   @Test(expected = NullPointerException.class)
@@ -23,5 +27,13 @@ public class PreconditionsTests {
   @Test public void shouldReturnTheSame() {
     String what = Preconditions.checkNotNull("Hey", "Not-null");
     assertThat(what).isEqualTo("Hey");
+  }
+
+  @Test public void constructorShouldBePrivate()
+      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    Constructor<Preconditions> constructor = Preconditions.class.getDeclaredConstructor();
+    assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+    constructor.setAccessible(true);
+    constructor.newInstance();
   }
 }
