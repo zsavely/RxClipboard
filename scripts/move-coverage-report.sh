44b6fdfa1f86bea9fd6c736aefb5dfe3fd37ef6e
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-08-10 21:48:21 +0300

    Added moving 'report.xml' for saving first report.

--- scripts/move-coverage-report.sh
+++ scripts/move-coverage-report.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/outputs/code-coverage/connected/coverage.ec /home/travis/build/zsavely/RxClipboard/rxclipboard/build/coverage.ec
+mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/outputs/code-coverage/connected/coverage.ec /home/travis/build/zsavely/RxClipboard/rxclipboard/build/coverage_first.ec
+mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/debug/report.xml /home/travis/build/zsavely/RxClipboard/rxclipboard/build/report_first.xml
