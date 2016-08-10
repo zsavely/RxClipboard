abce2e18afb8f574c498edcb0dd16fc4187b2dd7
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-08-10 23:52:37 +0300

    Updated script so that codecov has release report.

--- scripts/move-coverage-report-back.sh
+++ scripts/move-coverage-report-back.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/coverage_first.ec /home/travis/build/zsavely/RxClipboard/rxclipboard/build/outputs/code-coverage/connected/coverage_first.ec
-mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/report_first.xml /home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/debug/report_first.xml
+
+# Try to trick codecov that we have both release and debug code coverage.
+mkdir -p foo/bar/baz/home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/release/
+mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/report_first.xml /home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/release/report.xml
