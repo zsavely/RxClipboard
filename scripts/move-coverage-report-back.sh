bc7f866a084f4e8dd785c926ef0a6f6176ce42ca
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-08-10 22:33:18 +0300

    Added moving 'report.xml' back.

---
+++ scripts/move-coverage-report-back.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/coverage_first.ec /home/travis/build/zsavely/RxClipboard/rxclipboard/build/outputs/code-coverage/connected/coverage_first.ec
+mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/report_first.xml /home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/debug/report_first.xml
